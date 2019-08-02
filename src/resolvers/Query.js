e626869c162680572aad90cf56ef56bcb64aa2cf
Gabriel Pereira <gabriel6562014@gmail.com>
2019-08-02 00:08:31 -0300

    Finalized jwt authentication

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -2,10 +2,7 @@ const User = require('../models/User');
 
 const Query = {
     user: (_, { id }) => User.findById(id),
-    users: () => {
-        console.log(process.env.SALT_KEY);
-        return User.find()
-    },
+    users: () => User.find(),
 };
 
 module.exports = Query;
