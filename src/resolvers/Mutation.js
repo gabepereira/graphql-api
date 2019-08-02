5ae67d28b29a706d9403d99a5ffd87c8b5ac4889
Gabriel Pereira <gabriel6562014@gmail.com>
2019-08-02 10:20:28 -0300

    Added error response to authentication resolver

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -6,21 +6,19 @@ const Mutation = {
     createUser: (_, { name, email, password }) => User.create({
 		name, email, password: md5(password + process.env.SALT_KEY)
 	}),
-		
+
 	auth: async(_, { email, password }) => {
 		const user = await User.findOne({
 			email, password: md5(password + process.env.SALT_KEY)
 		});
-
-		console.log(user);
-
+		
 		return user ? {
             token: jwt.sign({
 				id: user._id,
 				email: user.email,
 				name: user.name
 			}, process.env.SALT_KEY, { expiresIn: '1d' })
-		} : false;
+		} : new Error('No user found.');
 	},
 };
 
