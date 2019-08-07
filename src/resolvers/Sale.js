801ce3b4005464a6434515c782a504e3dc110210
Gabriel Pereira <gabriel6562014@gmail.com>
2019-08-06 21:41:58 -0300

    Created sales resolvers

--- src/resolvers/Sale.js
+++ src/resolvers/Sale.js
@@ -3,29 +3,27 @@ const Sale = require('../models/Sale');
 const Query = {
     sales: async(_, {}) => Sale.findById(id),
 
-    sales: async(_, {}) => {
+    sales: async(_, {}) => (
         await Sale.find({},
             'user status items')
         .populate('user', 'name')
-        .populate('items.product', 'title price');
-    }
+        .populate('items.product', 'title price')
+    ),
 };
 
 const Mutation = {
     createSale: async(_, { productId, quantity }, ctx, info) => {
         let sale = await Sale.create({
-                user: ctx.token.id,
-                status: true,
-                items: {
-                    product: productId,
-                    quantity
-                }
+            user: ctx.token.id,
+            status: true,
+            items: {
+                product: productId,
+                quantity
+            }
         });
         sale = await sale.populate('user', 'name')
+        .populate('items.product', 'title price')
         .execPopulate();
-
-        console.log(sale);
-
         return sale;
     }
 };
